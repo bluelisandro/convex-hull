42b634226a72bafb4ebfbb05646f45adb9ca2bac
bluelisandro <nano0117@gmail.com>
2023-02-16 03:38:00 -0500

    AI generated sorcery

--- convex_hull.py
+++ convex_hull.py
@@ -7,145 +7,129 @@ import itertools # for combinations
 EPSILON = sys.float_info.epsilon
 Point = Tuple[int, int]
 
-# Checks if all points in list are collinear
-# Lisandro: I think that this is easier than having args of only 3 points, because we can give it a list with any number of points by passing in [a, b, ...]
+# Function that checks if list of points are collinear
 def collinear(points: List[Point]) -> bool:
-    """ Given a list of points, returns True if and only if all of those points are collinear.
-    """
-    if len(points) <= 2:
+    if len(points) < 3:
         return True
-    for i in range(1, len(points) - 1):
-        if not collinear(points[i - 1], points[i], points[i + 1]):
+    x1, y1 = points[0]
+    x2, y2 = points[1]
+    for x3, y3 in points[2:]:
+        if (y3 - y2) * (x2 - x1) != (y2 - y1) * (x3 - x2):
             return False
     return True
-# def collinear(a: Point, b: Point, c: Point) -> bool:
-#     """
-#     Given three points a,b,c,
-#     returns True if and only if a,b,c are collinear
-#     (subject to floating-point precision)
-#     """
-#     return abs(triangle_area(a, b, c)) <= EPSILON
 
-def y_intercept(p1: Point, p2: Point, x: int) -> float:
-    """
-    Given two points, p1 and p2, an x coordinate from a vertical line,
-    compute and return the the y-intercept of the line segment p1->p2
-    with the vertical line passing through x.
-    """
-    x1, y1 = p1
-    x2, y2 = p2
-    slope = (y2 - y1) / (x2 - x1)
-    return y1 + (x - x1) * slope
+def orientation(p, q, r):
+    # Returns the orientation of the triplet (p, q, r).
+    # Returns 0 if they are collinear, 1 if clockwise, and 2 if counterclockwise.
+    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
+    if val == 0:
+        return 0  # Collinear
+    elif val > 0:
+        return 1  # Clockwise orientation
+    else:
+        return 2  # Counterclockwise orientation
 
+def compute_tangent(left_hull, right_hull, leftmost_point, rightmost_point, direction):
+    # Compute the tangent line between the leftmost point and the rightmost point of the two hulls
+    # in the specified direction (1 for upper tangent, -1 for lower tangent)
+    done = False
+    while not done:
+        done = True
+        if direction == 1:
+            for i in range(len(left_hull)):
+                if orientation(leftmost_point, rightmost_point, left_hull[i]) == -1:
+                    leftmost_point = left_hull[i]
+                    done = False
+                    break
+            for i in range(len(right_hull)):
+                if orientation(leftmost_point, rightmost_point, right_hull[i]) == 1:
+                    rightmost_point = right_hull[i]
+                    done = False
+                    break
+        else:
+            for i in range(len(left_hull)):
+                if orientation(leftmost_point, rightmost_point, left_hull[i]) == 1:
+                    leftmost_point = left_hull[i]
+                    done = False
+                    break
+            for i in range(len(right_hull)):
+                if orientation(leftmost_point, rightmost_point, right_hull[i]) == -1:
+                    rightmost_point = right_hull[i]
+                    done = False
+                    break
 
-def triangle_area(a: Point, b: Point, c: Point) -> float:
-    """
-    Given three points a,b,c,
-    computes and returns the area defined by the triangle a,b,c.
-    Note that this area will be negative if a,b,c represents a clockwise sequence,
-    positive if it is counter-clockwise,
-    and zero if the points are collinear.
-    """
-    ax, ay = a
-    bx, by = b
-    cx, cy = c
-    return ((cx - bx) * (by - ay) - (bx - ax) * (cy - by)) / 2
+    return leftmost_point if direction == 1 else rightmost_point
 
+def merge_hulls(left_hull: List[Point], right_hull: List[Point]) -> List[Point]:
+    # Merge two convex hulls into one convex hull
+    # Find the rightmost point of the left convex hull and the leftmost point of the right convex hull
+    leftmost_point = max(left_hull)
+    rightmost_point = min(right_hull)
 
-def is_clockwise(a: Point, b: Point, c: Point) -> bool:
-    """
-    Given three points a,b,c,
-    returns True if and only if a,b,c represents a clockwise sequence
-    (subject to floating-point precision)
-    """
-    return triangle_area(a, b, c) < -EPSILON
+    # Compute the upper and lower tangents
+    upper_tangent = compute_tangent(left_hull, right_hull, leftmost_point, rightmost_point, 1)
+    lower_tangent = compute_tangent(left_hull, right_hull, leftmost_point, rightmost_point, -1)
 
+    # Remove interior points from both convex hulls
+    new_left_hull = left_hull[left_hull.index(lower_tangent):left_hull.index(upper_tangent)+1]
+    new_right_hull = right_hull[right_hull.index(upper_tangent):] + right_hull[:right_hull.index(lower_tangent)+1]
 
-def is_counter_clockwise(a: Point, b: Point, c: Point) -> bool:
-    """
-    Given three points a,b,c,
-    returns True if and only if a,b,c represents a counter-clockwise sequence
-    (subject to floating-point precision)
-    """
-    return triangle_area(a, b, c) > EPSILON
-
-def clockwise_sort(points: List[Point]):
-    """
-    Given a list of points, sorts those points in clockwise order about their centroid.
-    Note: this function modifies its argument.
-    """
-    # get mean x coord, mean y coord
-    x_mean = sum(p[0] for p in points) / len(points)
-    y_mean = sum(p[1] for p in points) / len(points)
-
-    def angle(point: Point):
-        return (math.atan2(point[1] - y_mean, point[0] - x_mean) + 2 * math.pi) % (2 * math.pi)
-
-    points.sort(key=angle)
-    return
+    # Concatenate the two new convex hulls in counterclockwise order
+    return new_left_hull + new_right_hull
 
 
 def base_case_hull(points: List[Point]) -> List[Point]:
-    """ Base case of the recursive algorithm.
-    """
-    
-    # Compute combination of points
-    # combinations = itertools.combinations(points, 2)
+    # Compute hull using graham scan algorithm
+    # Returns the convex hull of a set of points using the Graham scan algorithm.
+    n = len(points)
 
-    # Stores list of circular pairs of points as list of tuples
-    # Example: [(1,2), (2,3), (3,1)] --> [ [(1, 2), (2, 3)], [(2, 3), (3, 1)], [(3, 1), (1, 2)] ]
-    combinations = [(point1, point2) for point1, point2 in zip(points, points[1:]+points[:1])]
-    # TODO Not sure if these sort of combinations are what we need
+    # Sort points by y-coordinate (and by x-coordinate in case of a tie)
+    sorted_points = sorted(points, key=lambda point: (point[1], point[0]))
 
-    for combination in combinations:
-        point1 = combination[0]
-        point2 = combination[1]
-        if collinear([point1, point2]):
-            continue
-        else:
-            # Compute cross product
-            cp = cross_product(point1, point2)
-            if cp < 0:
-                # Point 1 is on the hull
-                # Point 2 is not on the hull
-                pass
-            elif cp > 0:
-                # Point 1 is not on the hull
-                # Point 2 is on the hull
-                pass
-            else:
-                # Point 1 and Point 2 are collinear
-                pass
-    
-    """ get the combination of points (this is your line segments)
-        for every line segment:
-        point1, point2 = x
-        for every point:
-            if theyre collinear: ignore
-            do cross product
-            check if the cp is <0, >0 or is 0
-        if there was a line segment where there were no points on one side, then that segment is on the hull """
+    # Find the pivot point (the point with the lowest y-coordinate)
+    pivot = sorted_points[0]
 
-    return points
+    # Sort the remaining points by polar angle in counterclockwise order with respect to the pivot
+    polar_sorted_points = sorted(sorted_points[1:], key=lambda point: (orientation(pivot, point, (float('inf'), pivot[1])), -point[1], point[0]))
 
+    # Initialize the stack with the pivot and the first point in the sorted list
+    stack = [pivot, polar_sorted_points[0]]
+
+    # Iterate over the remaining points, adding them to the stack if they turn left or popping if they turn right
+    for i in range(1, n-1):
+        while len(stack) > 1 and orientation(stack[-2], stack[-1], polar_sorted_points[i]) != 2:
+            stack.pop()
+        stack.append(polar_sorted_points[i])
+
+    return stack
 
 def compute_hull(points: List[Point]) -> List[Point]:
-    """
-    Given a list of points, computes the convex hull around those points
-    and returns only the points that are on the hull.
-    """
-    # TODO: Implement a correct computation of the convex hull
-    #  using the divide-and-conquer algorithm
-    # TODO: Document your Initialization, Maintenance and Termination invariants.
-    if len(points) > 5:
-        if collinear(points):
-            return points
-        else:
-            # Divide
-            median = len(points) // 2
-            left = compute_hull(points[:median])
-            right = compute_hull(points[median:])
-            # Conquer
-            return merge_hulls(left, right)
+    # Sort points by x-coordinate
+    points.sort(key=lambda p: p[0])
 
-    return points
+    if len(points) <= 3:
+        return base_case_hull(points)
+    else:
+        # Divide
+        # Split points in half
+        mid = len(points) // 2
+        left_hull = points[:mid]
+        right_hull = points[mid:]
+
+        # Conquer
+        # Recursively find convex hulls for left and right
+        left_hull = compute_hull(left_hull)
+        right_hull = compute_hull(right_hull)
+    
+    return merge_hulls(left_hull, right_hull)
+
+# Psudeocode
+    # 1 Sort the set of points by their x-coordinate. If two points have the same x-coordinate, sort them by their y-coordinate.
+
+    # 2 If there are only three or fewer points in the set, compute the convex hull directly using a known algorithm (e.g., Graham's scan or Jarvis march). This is the base case for the recursion.
+
+    # 3 Divide the set of points into two roughly equal-sized sets, using a vertical line that divides the points in half.
+
+    # 4 Recursively compute the convex hull of each of the two sets. This can be done by repeating steps 1-3 on each of the two sets.
+
+    # 5 Merge the two convex hulls to obtain the final convex hull. This can be accomplished by connecting the rightmost point of the left convex hull to the leftmost point of the right convex hull, and continuing in a counterclockwise direction until the starting point is reached.
